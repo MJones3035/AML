8e99e5c2bbe6039f70472254e96ceb42d43ec6a9
MJones3035 <matthewjones3035@gmail.com>
2024-11-21 00:40:49 +0000

    Signup Backend Start

--- database.php
+++ database.php
@@ -5,6 +5,11 @@
     $db_name = "aml_db";
     $db_password = "";
 
+    define($DB_SERVER, "localhost");
+    define( $DB_USERNAME, "root");
+    define($DB_NAME, "aml_db");
+    define($DB_PASSWORD, "");
+
     try {
         $db_conn = mysqli_connect($db_server, $db_username, $db_password, $db_name);
     }
--- signUp.php
+++ signUp.php
@@ -10,10 +10,10 @@
             <div class="col-md-6">
                 <div class="login-form-container border p-4 rounded shadow">
                     <h2 class="text-center mb-4">Create Your Account</h2>
-                    <form>
+                    <form action="page.php" method="post">
                         <div class="form-group mb-3">
                             <label for="username" class="form-label">Username</label>
-                            <input type="text" class="form-control" id="username" placeholder="Enter a username">
+                            <input type="text" class="form-control" id="username" name="username" placeholder="Enter a username">
                         </div>
                         <div class="form-group mb-3">
                             <label for="firstname" class="form-label">First Name</label>
@@ -29,7 +29,7 @@
                             <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                         </div>
                         <div class="form-group mb-3">
-                            <label for="number" class="form-label">Number</label>
+                            <label for="number" class="form-label">Telephone Number</label>
                             <input type="tel" class="form-control" id="number" placeholder="Enter your telephone number">
                         </div>
                         <div class="form-group mb-3">
@@ -52,7 +52,7 @@
                             <input type="checkbox" class="form-check-input" id="exampleCheck1">
                             <label class="form-check-label" for="exampleCheck1">Remember me</label>
                         </div> -->
-                        <button type="submit" class="btn btn-primary w-100">Sign Up</button>
+                        <button type="submit" class="btn btn-primary w-100" name="signup" value="true">Sign Up</button>
                     </form>
                 </div>
             </div>
@@ -63,3 +63,14 @@
 </body>
 
 </html>
+
+<?php 
+if(isset($_POST["signup"])) 
+{
+    $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+
+    echo $username;
+
+
+}
+?>
