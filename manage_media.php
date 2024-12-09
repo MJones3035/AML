21dda9ea0edc79d550b98460f4dd1ca794d4b552
ahmedobaid <aobaid2004@outlook.com>
2024-12-09 13:56:17 +0000

    started manage media page

---
+++ manage_media.php
@@ -0,0 +1,26 @@
+<?php
+include_once("user_sql.php");
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<?php require_once("header.php"); ?>
+<body class="d-flex flex-column ">
+    <div class="container">
+        <col class="col-12">
+            <h1>Manage Media</h1>
+            <table class="table table-striped">
+                <thead>
+                    <th>
+                        <td>Media ID</td>
+                        <td>Title</td>
+                        <td>Author</td>
+                        <td>Year</td>
+                        <td>Genre</td>
+                        <td>Type</td>
+                    </th>
+                </thead>
+</body>
